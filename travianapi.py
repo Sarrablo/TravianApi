0d484068d23da27a4842600d43588c1c3912c23c
Gonzalo Sarrablo <gsarrablo@gmail.com>
2017-08-10 11:16:53 +0200

    Added domain to constructor and fixed bug

--- travianapi.py
+++ travianapi.py
@@ -3,14 +3,15 @@ import re
 from bs4 import BeautifulSoup
 class TravianGuerrillaApi:
 
-    def __init__(self, user, pasw, server):
+    def __init__(self, user, pasw, server, domain='net'):
         self.browser = RoboBrowser(history=True,parser='html.parser')
         self.browser.open('https://www.travian.net/')
         self.server = server.strip()
+        self.domain = domain
         self.loggin(user, pasw)
 
     def loggin(self, user, pasw):
-        url = 'https://%s.travian.net/'%(self.server)
+        url = 'https://%s.travian.%s/'%(self.server,self.domain)
         self.browser.open(url)
         logging_form = self.browser.get_form(action="dorf1.php")
         
@@ -25,7 +26,7 @@ class TravianGuerrillaApi:
         print(page)
     
     def show_available_units(self,solar_id):
-        url = 'https://%s.travian.net/build.php?id=%s'%(self.server,solar_id)
+        url = 'https://%s.travian.%s/build.php?id=%s'%(self.server, self.domain,solar_id)
         self.open_page(url)
         
         soup = BeautifulSoup(str(self.browser.parsed), 'html.parser')
@@ -36,7 +37,7 @@ class TravianGuerrillaApi:
             print("%s -> %s Available"%(name,quant_available))
 
     def create_units(self, solar_id, t1=0,t2=0,t3=0,t4=0,t5=0):
-        url = 'https://%s.travian.net/build.php?id=%s'%(self.server,solar_id)
+        url = 'https://%s.travian.%s/build.php?id=%s'%(self.server,self.domain,solar_id)
         self.open_page(url)
         try:
             search_form = self.browser.get_form(action=re.compile(r'build.php'))
@@ -47,7 +48,7 @@ class TravianGuerrillaApi:
             return "Unavailable"
 
     def get_next_atack(self):
-        url = 'https://%s.travian.net/dorf1.php'%(self.server)
+        url = 'https://%s.travian.%s/dorf1.php'%(self.server,self.domain)
         self.open_page(url)
         soup = BeautifulSoup(str(self.browser.parsed), 'html.parser')
         seconds = soup.find('table',{'id':'movements'}).find('span',{'class':'timer'})['value']
@@ -69,7 +70,7 @@ class TravianGuerrillaApi:
             return True
 
     def actual_queue(self):
-        url = 'https://%s.travian.net/dorf1.php'%(self.server)
+        url = 'https://%s.travian.%s/dorf1.php'%(self.server,self.domain)
         self.open_page(url)
         try:
             soup = BeautifulSoup(str(self.browser.parsed), 'html.parser')
@@ -87,7 +88,7 @@ class TravianGuerrillaApi:
     def build_resource(self, resource_id):
         if not self.is_busy():
             try:
-                self.open_page('https://%s.travian.net/build.php?id=%s'%(self.server, resource_id))
+                self.open_page('https://%s.travian.%s/build.php?id=%s'%(self.server,self.domain, resource_id))
                 onclick = self.browser.select('.green.build')[0].attrs['onclick']
                 link = re.match(".*'(.*)'.*", onclick).group(1)
                 url = 'https://%s.travian.net/%s'%(self.server, link)
@@ -98,7 +99,7 @@ class TravianGuerrillaApi:
             return "Full queue"
 
     def show_avilable_building(self,solar_id,category_id=1):
-        self.open_page('https://%s.travian.net/build.php?id=%s&category=%s'%(self.server, solar_id, category_id))
+        self.open_page('https://%s.travian.%s/build.php?id=%s&category=%s'%(self.server,self.domain, solar_id, category_id))
         soup = BeautifulSoup(str(self.browser.parsed), 'html.parser')
         buildings = soup.find(id="build").find_all('div',{'class':'buildingWrapper'})
         for item in buildings:
@@ -112,28 +113,28 @@ class TravianGuerrillaApi:
 
     def build_building(self, solar_id, building_id ):
         try:            
-            self.open_page('https://%s.travian.net/build.php?id=%s&category=1'%(self.server, solar_id))
+            self.open_page('https://%s.travian.%s/build.php?id=%s&category=1'%(self.server,self.domain, solar_id))
             soup = BeautifulSoup(str(self.browser.parsed), 'html.parser')
             code = re.findall('c=.*\Z',re.match(".*'(.*)'.*",soup.find("button",{'class':'green new'}).attrs["onclick"]).group(1))[0][2:]
         
-            self.open_page('https://%s.travian.net/dorf2.php?a=%s&id=%s&c=%s'%(self.server, building_id, solar_id, code))
+            self.open_page('https://%s.travian.%s/dorf2.php?a=%s&id=%s&c=%s'%(self.server,self.domain, building_id, solar_id, code))
             return 'Ok'
         except:
             return 'Unavailable'
 
     def upgrade_building(self, solar_id):
         try:
-            self.open_page('https://%s.travian.net/build.php?id=%s'%(self.server, solar_id))
+            self.open_page('https://%s.travian.%s/build.php?id=%s'%(self.server, self.domain, solar_id))
             soup = BeautifulSoup(str(self.browser.parsed), 'html.parser')
             link = re.match(".*'(.*)'.*",soup.find("button",{'class':'green build'}).attrs["onclick"]).group(1)
-            url = 'https://%s.travian.net/%s'%(self.server, link)
+            url = 'https://%s.travian.%s/%s'%(self.server,self.domain, link)
             self.open_page(url)
         except:
            return "Unavailable"
 
 
     def get_actual_production(self):
-        url = 'https://%s.travian.net/dorf1.php'%(self.server)
+        url = 'https://%s.travian.%s/dorf1.php'%(self.server,self.domain)
         self.open_page(url)
         production = self.browser.find(id="production").find_all("tr")
         for item in production:
@@ -148,7 +149,7 @@ class TravianGuerrillaApi:
                 print('%s -> %s'%(resource,ammount))
     
     def map_resources(self):
-        url = 'https://%s.travian.net/dorf1.php'%(self.server)
+        url = 'https://%s.travian.%s/dorf1.php'%(self.server,self.domain)
         self.open_page(url)
         resource_map = self.browser.find(id="rx").find_all("area")
         for item in resource_map:
@@ -157,7 +158,7 @@ class TravianGuerrillaApi:
             except:
                 pass
     def map_buildings(self):
-        url = 'https://%s.travian.net/dorf2.php'%(self.server)
+        url = 'https://%s.travian.%s/dorf2.php'%(self.server,self.domain)
         self.open_page(url)
         building_map = self.browser.find(id="clickareas").find_all("area")
         for item in building_map:
@@ -167,7 +168,7 @@ class TravianGuerrillaApi:
                 pass
 
     def actual_resources(self):
-        url = 'https://%s.travian.net/dorf1.php'%(self.server)
+        url = 'https://%s.travian.%s/dorf1.php'%(self.server,self.domain)
         self.open_page(url)
         actual_resources = self.browser.find(id="stockBar").find_all("li")
         actual_resources_list=[]
